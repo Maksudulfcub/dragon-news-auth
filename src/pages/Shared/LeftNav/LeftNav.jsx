793fb5661b746fe215540aaf2e494886f0d1abdb
Maksudulfcub <maaksuduldish@gmail.com>
2024-04-05 21:14:00 +0600

    Left navbar added

--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -1,8 +1,22 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftNav = () => {
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch('categories.json')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, [])
+
     return (
         <div>
-            <h1 className="text-4xl">Left side Nav</h1>
+            <h1 className="text-xl font-semibold">All Categories</h1>
+            {
+                categories.map(category => <Link className="block p-4 text-xl font-medium text-gray-500 hover:bg-gray-200 rounded-xl hover:font-semibold" to={`/category/${category.id}`} key={category.id}>{category.name}</Link> )
+            }
         </div>
     );
 };
